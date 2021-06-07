34750bbd56d6f9b5f279523cbc8a04f1296fc3ae
Atherv Patil <athervpatil05@gmail.com>
2021-06-08 00:04:53 +0530

    selection-sort-algorithm

--- Algorithms/Sorting-Algorithms/Selection_Sort.py
+++ Algorithms/Sorting-Algorithms/Selection_Sort.py
@@ -2,9 +2,10 @@
 # Time Complexity = O(n^2), Space Complexity = O(1)
 
 def selection_sort(arr):
-    for i in range(0, len(arr)):
+    n = len(arr)
+    for i in range(0, n):
         minimum_index = i
-        for j in range(i+1, len(arr)):
+        for j in range(i+1, n):
             if arr[minimum_index] > arr[j]:
                 minimum_index = j
         arr[i],arr[minimum_index] = arr[minimum_index],arr[i]
